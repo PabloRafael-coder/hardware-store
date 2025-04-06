594ff396acd7bbf483b5d4a4620d0c7cb04cb915
Pablo Rafael <pabloofael@gmail.com>
2025-04-05 21:47:33 -0300

    feat(Button): Adicionar novos estilos

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
-  background-color: rgb(0, 0, 0);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+  background-color: ${(props) => props.theme['--black']};
   border: none;
-  border-radius: 0.25rem;
-  padding: 0.625rem;
+  border-radius: 4px;
+  padding: 0.5rem;
   font-size: 1rem;
   font-weight: 500;
   cursor: pointer;
-  color: #e3e3e3;
+  color: ${(props) => props.theme['--gray-100']};
   width: 100%;
 
   &:hover {
