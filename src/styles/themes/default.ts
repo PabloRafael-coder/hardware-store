42c9359442e200408c5653761a55a9a0aa3b2aa2
Pablo Rafael <pabloofael@gmail.com>
2025-04-05 21:47:12 -0300

    feat: Adicionar nova cor em --base-hover

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,7 +1,7 @@
 export const defaultThemes = {
   '--gray-100': '#f3f4f6',
   '--base-border': '#e5e7eb',
-  '--base-hover': '#d1d5db',
+  '--base-hover': '#f4f4f5',
   '--gray-400': '#9ca3af',
   '--base-link': '#52525b',
   '--gray-600': '#4b5563',
